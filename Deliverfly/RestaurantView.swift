b25b2d3a4a255fb5e0a2a6cd6bc2989a9c309886
Larisa Khachatryan <larisa.k@tumo.org>
2024-06-30 11:31:25 +0400

    Deliverfly

---
+++ Deliverfly/RestaurantView.swift
@@ -0,0 +1,78 @@
+//
+//  RestaurantView.swift
+//  Deliverfly
+//
+//  Created by user on 6/17/24.
+//
+
+import SwiftUI
+
+struct RestaurantView: View {
+    let restaurant : Restaurant
+    @State private var selectedFood: Food?
+    @EnvironmentObject private var navigation : Navigation
+    var body: some View {
+        ScrollView {
+            VStack (alignment: .leading){
+                HStack{
+                    Button (action: {
+                        navigation.goBack()
+                    }, label : {
+                        Image(.backArrow)
+                            .frame (width: 50,height:50)
+                            .background(.lightGray)
+                            .clipShape(Circle())
+                            .padding(.trailing)
+                    })
+                    Text ("Restaurant")
+                    Spacer ()
+                }
+                Image(restaurant.image)
+                    .resizable()
+                    .scaledToFill()
+                    .frame (height:150)
+                    .clipShape(RoundedRectangle(cornerRadius: 15))
+                    .padding(.vertical )
+                    .allowsHitTesting(/*@START_MENU_TOKEN@*/false/*@END_MENU_TOKEN@*/)
+                Text (restaurant.name)
+                    .font (.title2)
+                    .bold()
+                    .foregroundStyle(.darkBlue)
+                    .padding(.vertical,5)
+                Text (restaurant.description)
+                    .font (.subheadline)
+                    .lineSpacing(10)
+                    .foregroundStyle(.gray)
+                Text ("Menu")
+                    .font (.title)
+                    .padding (.vertical)
+                LazyVGrid(columns: [GridItem(.adaptive(minimum: 150))], content: {
+                    ForEach(restaurant.foods, id: \.self) { food in
+                        Button {
+                            selectedFood = food
+                        } label: {
+                            FoodPreview(food: food)
+                                .frame(height: 200)
+                        }
+                        
+                    }
+                })
+                
+            }
+            .padding(.horizontal)
+        }
+        .sheet(item: $selectedFood) { item in
+            foodView(item)
+        }
+    }
+    @ViewBuilder func foodView(_ item: Food) -> some View {
+        FoodView(food: item)
+            .presentationDetents(item.ingredients.isEmpty ? [.fraction(0.63)]:[.fraction(0.93)])
+            .presentationDragIndicator(.visible)
+            .presentationCornerRadius(30)
+    }
+}
+
+#Preview {
+    RestaurantView(restaurant: .inNOut)
+}
